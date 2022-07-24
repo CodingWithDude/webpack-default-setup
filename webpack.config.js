5965e02c1ca4d206b91e76e84bfcb014b7b4be91
CodingWithDude <codingwithdude@gmail.com>
2022-07-23 22:26:55 -0700

    Updated scss to sass

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,10 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
